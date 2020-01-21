22934b06de49918aa6318a7bcf70bcabe971508c
keithcruz <keith.cruz@outlook.com>
2020-01-20 22:38:46 -0600

    add default value to list

--- user/models.py
+++ user/models.py
@@ -5,7 +5,7 @@ from gif.models import Gif
 class User(db.Document):
     email = db.EmailField(required=True, unique=True)
     password = db.StringField(required=True, min_length=6)
-    gifs = db.ListField(db.EmbeddedDocumentField(Gif))
+    gifs = db.ListField(db.EmbeddedDocumentField(Gif), default=list)
 
     def hash_password(self):
         self.password = bcrypt.generate_password_hash(
