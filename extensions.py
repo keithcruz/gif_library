2387f085d161cfb692164141b8bb04b0ab62edd3
keithcruz <keith.cruz@outlook.com>
2020-01-22 22:06:09 -0600

    enable cors

--- extensions.py
+++ extensions.py
@@ -1,11 +1,13 @@
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_mongoengine import MongoEngine
 
-from giphyApi import GiphyApi
+from giphy_api import GiphyApi
 
 
 bcrypt = Bcrypt()
 giphyApi = GiphyApi()
 jwt = JWTManager()
 db = MongoEngine()
+cors = CORS()
