e70c302a60ad926bc9f6ac49e56763963edec23d
keithcruz <keith.cruz@outlook.com>
2020-01-25 22:42:43 -0600

    adding pagination to search results

--- gif/gifs.py
+++ gif/gifs.py
@@ -7,7 +7,10 @@ from extensions import giphyApi
 
 blueprint = Blueprint("gif", __name__)
 
-search_args = {"q": fields.Str(required=True)}
+search_args = {
+    "q": fields.Str(required=True),
+    "offset": fields.Int()
+}
 
 get_args = {"ids": fields.Str(required=True)}
 
@@ -16,7 +19,7 @@ get_args = {"ids": fields.Str(required=True)}
 @use_args(search_args, locations=["query"])
 @jwt_required
 def search_gifs(args):
-    results = giphyApi.search(args.get("q"))
+    results = giphyApi.search(args.get("q"), args.get("offset"))
     return results, 200
 
 
